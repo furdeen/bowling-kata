280440a6c50a65d4b78f82c2a1ebe22393fb57f0
Faisal Mumtaz <fascaptech@gmail.com>
2023-11-06 23:54:44 +0000

    feat : refactor code

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,37 @@
+//Bowling Kata
+// test cases :
+// all zero score
+// all one score
+// full score
+// spare score
+// strike score
+// all spare score
+// all strike score
+// all spare score with bonus
 
-export function allGuter(): number {
-    const array = new Array(20).fill(0);
-    console.log(array);
 
-    let score =0
-    for(let i = 0; i < array.length; i++){
-        score = score + array[i];
-        
-      }
-
-      return score;
- 
-
-     //const score = array[0]+array[1]+array[2]+array[3]+array[4]+array[5]+array[6]+array[7]+array[8]+array[9]+array[10]+array[11]+array[12]+array[13]+array[14]+array[15]+array[16]+array[17]+array[18]+array[19];
-    //return score;
-    
+export function allGuter() : number {
+  let score = 0;
+ //const rolls = [0,0,0,0,0,0,0,0,0,0];
+ const frames = Array(10).fill(0);
+  for (let i = 0; i < frames.length; i++) {
+    score += frames[i];
+  }
   
-}
-export function singleKnock(): number {
+  return score;
 
-  const array = new Array(20).fill(1);
-  console.log(array);
-    console.log(array);
-    let score1 =0
-    for(let i = 0; i < array.length; i++){
-        score1 = score1 + array[i];
-        
-      }
+  }
+  
 
-      return score1;
-
-}
+  export function allSingleScore() : number {
+    let score = 0;
+   //const rolls = [0,0,0,0,0,0,0,0,0,0];
+   const rolls = Array(20).fill(1);
+    for (let i = 0; i < rolls.length; i++) {
+      score += rolls[i];
+    }
+    
+    return score;
+  
+    }
+    
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,14 +1,14 @@
-import { allGuter, singleKnock } from "../src/index";
+//test bowilng kata 
+//all zero score
 
-describe("test guter function", () => {
-    it("should return 0", () => {
-        expect(allGuter()).toBe(0);
-    });
-});
+import { allGuter ,allSingleScore} from "../src/index";
 
-describe("test single score function", () => {
-    it("should return 20", () => {
-       // let score = new Array(20).fill(0);
-        expect(singleKnock()).toBe(20);
-    });
+describe("test allGuter function", () => {
+it("should return 0 for allGuter()", () => {
+expect(allGuter()).toBe(0);
 });
+})
+describe("test allSingleScore function", () => {
+    it("should return 20 for allsingleScore()", () => {
+    expect(allSingleScore()).toBe(20); });
+    })
