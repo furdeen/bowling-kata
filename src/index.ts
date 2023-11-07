fbb2b8dcc5b9cfdda3e060d7a87cba45f2a0d0bb
Faisal Mumtaz <fascaptech@gmail.com>
2023-11-07 15:28:02 +0000

    feat: add new test for spare score

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,8 @@
 // all strike score
 // all spare score with bonus
 
+import exp from "constants";
+
 
 export function allGuter() : number {
   let score = 0;
@@ -35,3 +37,54 @@ export function allGuter() : number {
   
     }
     
+    export function allSpareScore() : number {  
+      let score = 0;
+      const rolls = Array(21).fill(5);
+      for (let i = 0; i < rolls.length; i++) {
+        score += rolls[i];
+      }
+      console.log(score);
+      return score;
+    
+      }
+      export function allStrikeScore() : number {  
+        let score = 0;
+        const rolls = Array(12).fill(10);
+        for (let i = 0; i < rolls.length; i++) {
+          score += rolls[i];
+        }
+        console.log(score);
+        return score;
+      
+        }
+        export function rollsScore() : Array<number> {  
+          const rolls =[
+            0,0, //frame 1
+            0,0, //frame 2
+            0,0,// frame 3
+            0,0, //frame 4
+            0,0,  //frame 5
+            0,0,  //frame 6  
+            0,0, //frame 7
+            0,0, //frame 8
+            0,0, //frame 9   
+            3,7,8 //frame 10
+          ]
+          return rolls;
+          }
+          
+        export function calcScore(rollsScore:Array<number>) : any {  
+          let score = 0;
+          for (let frame = 0; frame < 10 ; frame++) {
+            const frameIndex = frame * 2;
+            const firstRoll = rollsScore[frameIndex];
+            const secondRoll = rollsScore[frameIndex + 1];
+            score += firstRoll + secondRoll;
+            if(firstRoll + secondRoll === 10){
+              score += rollsScore[frameIndex + 2];
+            }
+          }
+          console.log(score);
+          return score;
+        
+          }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,7 @@
 //test bowilng kata 
 //all zero score
 
-import { allGuter ,allSingleScore} from "../src/index";
+import { allGuter, allSingleScore, calcScore, rollsScore} from "../src/index";
 
 describe("test allGuter function", () => {
 it("should return 0 for allGuter()", () => {
@@ -11,4 +11,9 @@ expect(allGuter()).toBe(0);
 describe("test allSingleScore function", () => {
     it("should return 20 for allsingleScore()", () => {
     expect(allSingleScore()).toBe(20); });
-    })
+    });
+
+    describe("test calcScore function", () => {
+        it("should return 18 for calcScore()", () => {
+        expect(calcScore(rollsScore())).toBe(18); });
+        });
